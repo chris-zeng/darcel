f72c41f5838c4e02a7900086b8aa816eace7ce4e
wei <wei@spiretrading.com>
2017-05-03 14:25:51 -0400

    finished preliminary version of expression parser, still need to handle Arrays

--- expression_parser.py
+++ expression_parser.py
@@ -1,54 +1,41 @@
 from pyparsing import *
 
- 
-point = Literal('.')
-plus_op = Literal('+')
-minus_op = Literal('-')
-alpha = Word(alphas + '_')
-number = Word(nums)
-integer = Combine(Optional(plus_op|minus_op) + number)
-decimal = Combine(integer+Optional(point + Optional(number)))
-money = Combine('$' + decimal)
-true = Literal("True")
-false = Literal("False")
-boolean = true | false
-multi_op = Literal('*')
-divide_op = Literal('/')
-arithmetic_op = plus_op | minus_op | multi_op | divide_op
-assign_op = Literal('=')
-greater_op = Literal('>')
-less_op = Literal('<')
-greater_equal_op = Literal('>=')
-less_equal_op = Literal('<=')
-equal_equal_op = Literal('==')
-not_equal_op = Literal('!=')
-comp_op = (greater_op | less_op | greater_equal_op | less_equal_op |
-    equal_equal_op | not_equal_op)
-and_op = Literal("&&")
-or_op = Literal("||")
-binary_op = and_op | or_op
-left_paren = Suppress(Literal('('))
-right_paren = Suppress(Literal(')'))
-identifier = Combine(alpha + ZeroOrMore(alpha|number))
-qualified_name = Combine(identifier + ZeroOrMore(point+identifier))
-ops = arithmetic_op|multi_op|comp_op|assign_op|binary_op
-
-expr = Forward()
-atom = integer | money | decimal | boolean| expr | qualified_name
-
-expr << ((identifier + assign_op + atom + ZeroOrMore(ops + atom)) | \
-        (qualified_name + left_paren + Optional(atom + ZeroOrMore(Suppress(',')+atom)) + right_paren))
-
-#bnf = Optional(identifier + assign_op)+expr
-
-input=[]
-input.append("class()")
-input.append("a=1+1")
-input.append("a=1+1+1+1+1")
-input.append("a=1+1--1+1")
-input.append("class(a)")
-input.append("class(a, b)")
-input.append("class(a, c)")
-
-for i in input:
-    print expr.parseString(i)
+class ExpressionParser:
+    def parse_expression(self, input):
+        point = Literal('.')
+        plus_op = Literal('+')
+        minus_op = Literal('-')
+        alpha = Word(alphas + '_')
+        number = Word(nums)
+        integer = Combine(Optional(plus_op|minus_op) + number)
+        decimal = Combine(integer+Optional(point + Optional(number)))
+        money = Combine('$' + decimal)
+        true = Literal("True")
+        false = Literal("False")
+        boolean = true | false
+        multi_op = Literal('*')
+        divide_op = Literal('/')
+        arithmetic_op = plus_op | minus_op | multi_op | divide_op
+        assign_op = Literal('=')
+        greater_op = Literal('>')
+        less_op = Literal('<')
+        greater_equal_op = Literal('>=')
+        less_equal_op = Literal('<=')
+        equal_equal_op = Literal('==')
+        not_equal_op = Literal('!=')
+        comp_op = (greater_op | less_op | greater_equal_op | less_equal_op |
+            equal_equal_op | not_equal_op)
+        and_op = Literal("&&")
+        or_op = Literal("||")
+        binary_op = and_op | or_op
+        left_paren = Suppress(Literal('('))
+        right_paren = Suppress(Literal(')'))
+        identifier = Combine(alpha + ZeroOrMore(alpha|number))
+        qualified_name = Combine(identifier + ZeroOrMore(point+identifier))
+        ops = arithmetic_op|multi_op|comp_op|assign_op|binary_op
+        expr = Forward()
+        atom = integer | money | decimal | boolean | qualified_name
+        expr << (atom ^ (identifier + assign_op + expr + ZeroOrMore(ops + expr)) ^ \
+                (qualified_name + left_paren + Optional(
+                expr + ZeroOrMore(Suppress(',')+expr)) + right_paren))
+        return expr.searchString(input)
---
+++ expression_parser_unit_test.py
@@ -0,0 +1,49 @@
+import expression_parser
+import os
+import unittest
+
+
+class ExpressionParserUnitTest(unittest.TestCase):
+    def setUp(self):
+        return
+        
+    def tearDown(self):
+        return
+        
+    def test_1(self):
+        input=[]
+        input.append("class()")
+        input.append("a=1+1")
+        input.append("a=1+1+1+1+1")
+        input.append("a=1+1--1+1")
+        input.append("class(a)")
+        input.append("class(a, b)")
+        input.append("class(a=True, c)")
+        input.append("class(a=1+1+1+1+1)")
+        input.append("class(a=1+1+1+1+1, b)")
+        EP = expression_parser.ExpressionParser()
+        result=[]
+        for i in input:
+            result.append(str(EP.parse_expression(i)))
+        
+        self.assertEquals("[['class']]",result[0])
+        self.assertEquals("[['a', '=', '1', '+', '1']]", result[1])
+        self.assertEquals("[['a', '=', '1', '+', '1', '+', '1', '+', "+\
+            "'1', '+', '1']]", result[2])
+        self.assertEquals("[['a', '=', '1', '+', '1', '-', '-1', '+', '1']]", 
+            result[3])
+        self.assertEquals("[['class', 'a']]", result[4])
+        self.assertEquals("[['class', 'a', 'b']]", result[5])
+        self.assertEquals("[['class', 'a', '=', 'True', 'c']]", result[6])
+        self.assertEquals("[['class', 'a', '=', '1', '+', '1', '+', '1'," + \
+            " '+', '1', '+', '1']]", result[7])
+        self.assertEquals("[['class', 'a', '=', '1', '+', '1', '+', '1', " + \
+            "'+', '1', '+', '1', 'b']]", result[8])
+        
+           
+            
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
